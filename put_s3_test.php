3dce2615f43d1035d418b4b00a3268af0dfbd6f3
Leon <leon.stafford@mac.com>
2018-06-25 14:52:18 +0930

    space after 'SHA256' required

--- put_s3_test.php
+++ put_s3_test.php
@@ -7,6 +7,7 @@ define('S3_REGION', getenv('S3_REGION'));
 define('S3_BUCKET', getenv('S3_BUCKET'));
 
 
+
 // set the curl endpoint we will hit at AWS
 //$endpoint_url = "http://s3-" . S3_REGION . ".amazonaws.com/" . S3_BUCKET . "/index.html";
 $endpoint_url = "https://s3-" . S3_REGION . ".amazonaws.com/" . S3_BUCKET . "/index.html";
@@ -29,14 +30,15 @@ curl_setopt_array($curl, array(
 	CURLOPT_VERBOSE => true,
 	CURLOPT_STDERR =>  fopen('php://stderr', 'w'),
 	CURLOPT_HTTPHEADER => array(
+		'PUT',
 		'Content-Type: text/html',
 		'Content-Length: ' . filesize('index.html'),
 		'Content-MD5: ' . base64_encode(md5_file('index.html')),
 		'x-amz-acl: public-read',
 		'x-amz-date: 20180625',
 		'Host:' . S3_BUCKET . '.s3.amazonaws.com',
-		//'Date: 20180625',
-		'Authorization: AWS4-HMAC-SHA256Credential=' . S3_KEY . '/20180625/' . S3_REGION . '/s3/aws4_request,SignedHeaders=host;x-amz-date,Signature=fe5f80f77d5fa3beca038a248ff027d0445342fe2855ddc963176630326f1024'
+		'Date: 20180625',
+		'Authorization: AWS4-HMAC-SHA256 Credential=' . S3_KEY . '/20180625/' . S3_REGION . '/s3/aws4_request,SignedHeaders=host;x-amz-date,Signature=fe5f80f77d5fa3beca038a248ff027d0445342fe2855ddc963176630326f1024'
 	)
 ));
 
