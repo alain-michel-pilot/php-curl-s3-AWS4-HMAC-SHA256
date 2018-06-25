6bddb906f736f7470128016ee1315c74abd6d9ce
Leon <leon.stafford@mac.com>
2018-06-25 14:34:32 +0930

    timing out

--- put_s3_test.php
+++ put_s3_test.php
@@ -8,6 +8,7 @@ define('S3_BUCKET', getenv('S3_BUCKET'));
 
 
 // set the curl endpoint we will hit at AWS
+//$endpoint_url = "http://s3-" . S3_REGION . ".amazonaws.com/" . S3_BUCKET . "/index.html";
 $endpoint_url = "https://s3-" . S3_REGION . ".amazonaws.com/" . S3_BUCKET . "/index.html";
 
 // instantiate a curl resource
@@ -15,7 +16,6 @@ $curl = curl_init();
 
 
 // set our cURL options
-
 curl_setopt_array($curl, array(
 	CURLOPT_CONNECTTIMEOUT => 30,
 	CURLOPT_LOW_SPEED_LIMIT => 1,
@@ -23,24 +23,26 @@ curl_setopt_array($curl, array(
 	CURLOPT_USERAGENT => 'leonstafford/wordpress-static-html-plugin',
 	CURLOPT_URL => $endpoint_url,
 	CURLOPT_HEADER => true,// get header back in response
-	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_RETURNTRANSFER => false,
 	CURLOPT_FOLLOWLOCATION => false,
-	CURLOPT_VERBOSE => false,
-	CURLOPT_STDERR, fopen('php://stderr', 'w'),
-	CURLOPT_HTTPHEADER, array(
-		'PUT',
+	CURLOPT_CUSTOMREQUEST => "PUT",
+	CURLOPT_VERBOSE => true,
+	CURLOPT_STDERR =>  fopen('php://stderr', 'w'),
+	CURLOPT_HTTPHEADER => array(
 		'Content-Type: text/html',
+		'Content-Length: ' . filesize('index.html'),
 		'Content-MD5: ' . base64_encode(md5_file('index.html')),
 		'x-amz-acl: public-read',
-		'Host: BucketName.s3.amazonaws.com',
+		'x-amz-date: 20180625',
+		//'Host:' . S3_BUCKET . '.s3.amazonaws.com',
 		'Date: 20180625',
-		'Content-Length: ',
-		'Authorization: AWS4-HMAC-SHA256',
-		'Credential=' . S3_KEY . '/20180625/' . S3_REGION . '/s3/aws4_request',
-		'SignedHeaders=host;range;x-amz-date',
-		'Signature=fe5f80f77d5fa3beca038a248ff027d0445342fe2855ddc963176630326f1024'
+		'Authorization: AWS4-HMAC-SHA256Credential=' . S3_KEY . '/20180625/' . S3_REGION . '/s3/aws4_request,SignedHeaders=host;x-amz-date,Signature=fe5f80f77d5fa3beca038a248ff027d0445342fe2855ddc963176630326f1024'
 	)
 ));
+
+
+
+// show the curl info before making the request
 
 $output = curl_exec($curl); 
 
